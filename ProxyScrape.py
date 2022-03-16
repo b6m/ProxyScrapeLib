12957fd0f3d5a24876fd969674a3a76b7aedc288
b6m <65096390+b6m@users.noreply.github.com>
2022-03-17 00:17:22 +0100

    Add files via upload

---
+++ ProxyScrape.py
@@ -0,0 +1,21 @@
+import requests
+import colorama
+from colorama import Fore
+import time
+
+proxies = open("./proxies.txt").read().splitlines()
+
+def scrape():
+    f = open("./proxies.txt", "a+")
+    try:
+        r = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000")
+        for proxy in r.text.split("\n"):
+            proxy = proxy.strip()
+            if proxy:
+                f.write(str(proxy)+"\n")
+        f.close()
+        print(f"{Fore.GREEN}[{Fore.RESET}SUCCESS{Fore.GREEN}]{Fore.RESET} Successfully scraped proxies.")
+    except:
+        print(f"{Fore.RED}[{Fore.RESET}ERROR{Fore.RED}]{Fore.RESET} Unable to scrape proxies, please try again later.")
+        time.sleep(2)
+        exit()
---
+++ main.py
@@ -0,0 +1,3 @@
+from ProxyScrape import *
+
+scrape()
