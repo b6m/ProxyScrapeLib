3441ca54ca7410b7fba29851646ea6530dd26e41
b6m <65096390+b6m@users.noreply.github.com>
2022-03-17 01:03:34 +0100

    Update ProxyScrape.py

--- ProxyScrape.py
+++ ProxyScrape.py
@@ -3,19 +3,23 @@ import colorama
 from colorama import Fore
 import time
 
-proxies = open("./proxies.txt").read().splitlines()
-
 def scrape():
-    f = open("./proxies.txt", "w")
+    f = open("./proxies.txt", "w+")
+    proxies = open("./proxies.txt").read().splitlines()
     try:
-        r = requests.get("https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000")
+        base_url = "https://api.proxyscrape.com/?request=displayproxies&proxytype=http&timeout=5000"
+        r = requests.get(base_url)
         for proxy in r.text.split("\n"):
             proxy = proxy.strip()
             if proxy:
                 f.write(str(proxy)+"\n")
         f.close()
-        print(f"{Fore.GREEN}[{Fore.RESET}SUCCESS{Fore.GREEN}]{Fore.RESET} Successfully scraped proxies.")
+        print(
+            f"{Fore.GREEN}[{Fore.RESET}SUCCESS{Fore.GREEN}]{Fore.RESET} Successfully Scraped Proxies"
+            )
     except:
-        print(f"{Fore.RED}[{Fore.RESET}ERROR{Fore.RED}]{Fore.RESET} Unable to scrape proxies, please try again later.")
+        print(
+            f"{Fore.RED}[{Fore.RESET}ERROR{Fore.RED}]{Fore.RESET} Couldn't Scrape Proxies"
+            )
         time.sleep(2)
         exit()
